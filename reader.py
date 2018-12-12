e82508f222b4f9c4e9c1ae2dc25b39c3eb8364cd
noahtren <noahtren@gmail.com>
2018-12-11 21:20:43 -0500

    system for reading daily diet file in .yaml format

---
+++ reader.py
@@ -0,0 +1,24 @@
+import yaml
+import settings
+from nutrition import *
+
+def read_day():
+    day_dict = yaml.load(open(settings.day_path, "r"))["day"]["meals"]
+    meal_objs = []
+    for meal in day_dict:
+        food_objs = []
+        for food in day_dict[meal]:
+            food_dict = day_dict[meal][food]
+            food_dict["meal"] = meal
+            if 'nutrients' not in food_dict.keys():
+                pass
+                food_objs.append(Food(access_database(food_dict["id"]), food_dict["grams"]))
+            else:
+                nutrient_objs = []
+                for nutrient in day_dict[meal][food]["nutrients"]:
+                    nutrient_objs.append(Nutrient(nutrient["name"],nutrient["group"],nutrient["value"],nutrient["unit"]))
+                food_objs.append(Food(food, nutrient_objs))
+        if len(food_objs) > 0:
+            meal_objs.append(Meal(food_objs))
+    day_obj = Day(meal_objs)
+    return day_obj
