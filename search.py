4dcbcd0a186ad85e9f8fefb40dfc1e9a3153d19d
noahtren <noahtren@gmail.com>
2018-12-15 18:57:30 -0500

    new implementation for access_database method

--- search.py
+++ search.py
@@ -1,11 +1,10 @@
 from nutrition import *
 
 # Create a result object based on a text search
-broccoli_results = Results(search("broccoli"))
-print(broccoli_results.Search_Info())
+chocolate = Results(search("chocolate"))
+print(chocolate.Search_Info())
 # Get the food object representing the first result
-first = broccoli_results.Get_Result(0)
-broccoli = Food(get_food_data(first),100)
+chocolate = Food(access_database("19081"),100)
 # Show the info of the first result from a search
-print(broccoli.Food_Info())
+print(chocolate.Food_Info())
 # you can also print broccoli.Food_Info(True) for an assessment of rda values
