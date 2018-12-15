66f15ec004a279ca35afcf5697bf0fde7c1c6e0f
noahtren <noahtren@gmail.com>
2018-12-15 00:59:47 -0500

    more detailed api call, more involved cleanup process

--- nutrition.py
+++ nutrition.py
@@ -2,6 +2,7 @@ import requests
 import json
 import operator
 import os
+import settings
 from profile import recommended_amounts
 
 local_usda_foods = []
@@ -120,10 +121,23 @@ class Food:
             first is name, second is list of nutrients
         '''
         if type(second) is int or type(second) is float:
-            nutrient_json = first["foods"][0]["food"]["nutrients"]
+            nutrient_json = first["nutrients"]
+            # parse the json nutrient file to only tracked nutrients
+            # in this case, the tracked nutrients are from the recommended amount dict
+            to_del = []
+            for n in nutrient_json:
+                if n["name"] not in recommended_amounts.keys():
+                    to_del.append(nutrient_json.index(n))
+            # this algorithm will never take too much time because a list of nutrients
+            # will never be greater than ~150
+            for i in to_del:
+                del nutrient_json[i]
+                for j in to_del:
+                    if i < j:
+                        to_del[to_del.index(j)] -= 1
             groups = [] # proximates, lipids, etc
             grouped_nutrients = []
-            self.name = first["foods"][0]["food"]["desc"]["name"]
+            self.name = first["desc"]["name"]
             for nutrient in nutrient_json:
                 if (float(nutrient["value"]) != 0):
                     if (nutrient["group"] not in groups):
@@ -133,7 +147,7 @@ class Food:
                     grouped_nutrients[len(groups)-1].append(Nutrient(nutrient["name"], nutrient["group"], float(nutrient["value"]), nutrient["unit"], second, self.name))
                     count += 1
             self.nutrients = grouped_nutrients
-            self.id = first["foods"][0]["food"]["desc"]["ndbno"]
+            self.id = first["desc"]["ndbno"]
             self.grams = second
             self.groups = groups
         else:
@@ -234,19 +248,65 @@ class Results:
         else:
             return "No results"
 
+name_filter = ["Energy", "Total lipid (fat)", "Carbohydrate, by difference",
+               "Fiber, total dietary", "Sugars, total", "Calcium, Ca", "Iron, Fe",
+               "Potassium, K", "Sodium, Na", "Magnesium, Mg", "Phosphorus, P",
+               "Zinc, Zn", "Copper, Cu", "Manganese, Mn",
+               "Selenium, Se", "Fluoride, F",
+               "Vitamin E (alpha-tocopherol)", "Vitamin K (phylloquinone)",
+               "Vitamin C, total ascorbic acid", "Folate, total", 
+               "Choline, total",
+               "Fatty acids, total saturated",
+               "Fatty acids, total monounsaturated", "Fatty acids, total polyunsaturated",
+               "Fatty acids, total trans"]
+name_replace = ["Calories", "Fat", "Carbs", "Fiber", "Sugar", "Calcium", "Iron",
+                "Potassium", "Sodium", "Magnesium", "Phosphorus",
+                "Zinc", "Copper", "Manganese",
+                "Selenium", "Fluoride",
+                "Vitamin E", "Vitamin K",
+                "Vitamin C", "Folate", 
+                "Choline", "Saturated",
+                "Monounsaturated", "Polyunsaturated",
+                "Trans"]
 def access_database(food_id):
+    if len(local_usda_foods) == 0:
+        scan_files()
     if food_id in local_usda_foods:
         response = open("usda_foods/{}.json".format(food_id), "r").read()
+        food_json = json.loads(response)
     else:
         params = dict(
             ndbno=str(food_id),
-            type='b', 
+            type='f', 
             format='json', 
             api_key=key
         )
         response = requests.get(url=url + "/V2/reports", params=params).text
-        open("usda_foods/{}.json".format(food_id), "w").write(response)
-    return json.loads(response)
+        food_json = json.loads(response)
+        food_json = food_json["foods"][0]["food"]
+        del food_json["sr"]; del food_json["type"]; del food_json["footnotes"]
+        del food_json["sources"]; del food_json["langual"]
+        desc = food_json["desc"]
+        extraneous_describers = ["sd", "sn", "cn", "manu", "nf", "cf", "ff", "pf", "r", "rd", "ds",
+                                 "ru"]
+        for describer in extraneous_describers:
+            if describer in desc:
+                del desc[describer]
+        to_del = []
+        for n in food_json["nutrients"]:
+            if n["name"] in name_filter:
+                n["name"] = name_replace[name_filter.index(n["name"])]
+            if n["name"] == "Calories" and n["unit"] == "kJ":
+                to_del.append(food_json["nutrients"].index(n))
+            del n["measures"]; del n["derivation"]; del n["nutrient_id"]
+            del n["sourcecode"]; del n["dp"]; del n["se"]
+        for i in to_del:
+            del food_json["nutrients"][i]
+            for j in to_del:
+                if i < j:
+                    to_del[to_del.index(j)] -= 1
+        open("usda_foods/{}.json".format(food_id), "w").write(json.dumps(food_json))
+    return food_json
 
 def search(name):
     params = dict(
@@ -261,7 +321,8 @@ def search(name):
     usda_data = json.loads(response)
     return usda_data
 
+def scan_files():
 # Update list of files that are in local storage
-cwd = os.getcwd()
-for f in os.scandir(cwd+"/usda_foods"):
-    local_usda_foods.append(str(f.name).split(".")[0])
+    cwd = os.getcwd()
+    for f in os.scandir(cwd+"/"+settings.usda_food_path):
+        local_usda_foods.append(str(f.name).split(".")[0])
