49035b1a658b26bfbf02df212372492fde741608
noahtren <noahtren@gmail.com>
2018-12-18 14:11:44 -0500

    listing individual rda values now works

--- search.py
+++ search.py
@@ -1,10 +1,12 @@
 from nutrition import *
 
 # Create a result object based on a text search
-chocolate = Results(search("water tap"))
+chocolate = Results(search("chocolate"))
 print(chocolate.Search_Info())
 # Get the food object representing the first result
 chocolate = Food(access_database("19081"),100)
+# That's a lot of chocolate!
+
 # Show the info of the first result from a search
 print(chocolate.Food_Info())
-# you can also print broccoli.Food_Info(True) for an assessment of rda values
+# you can also print chocolate.Food_Info(True) for an assessment of rda values
